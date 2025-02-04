be208217310ffffcbe0c89e58d62530bf06ad849
Danilo Xaxa <daniloxaxa01@gmail.com>
2025-02-04 20:20:30 -0300

    retirando ServiceOne (parceiros), MarqueeOne e subindo o TeamOne (clientes)

--- frontend/src/pages/HomeOne.jsx
+++ frontend/src/pages/HomeOne.jsx
@@ -3,13 +3,13 @@ import Preloader from "../elements/Preloader";
 import HelmetReact from "../elements/HelmetReact";
 import HeaderOne from "../components/HeaderOne";
 import HeroOne from "../components/HeroOne";
-import ServiceOne from "../components/ServiceOne";
+//import ServiceOne from "../components/ServiceOne";
 //import CTAOne from "../components/CTAOne";
 //import AboutOne from "../components/AboutOne";
 import ProcessOne from "../components/ProcessOne";
 //import PortfolioOne from "../components/PortfolioOne";
 //import TestimonialOne from "../components/TestimonialOne";
-import MarqueeOne from "../components/MarqueeOne";
+//import MarqueeOne from "../components/MarqueeOne";
 import TeamOne from "../components/TeamOne";
 import FaqOne from "../components/FaqOne";
 import BlogOne from "../components/BlogOne";
@@ -36,17 +36,11 @@ const HomeOne = () => {
       {/* HeroOne */}
       <HeroOne />
 
-      {/* ServiceOne */}
-      <ServiceOne />
+      {/* TeamOne */}
+      <TeamOne />
 
       {/* ProcessOne */}
       <ProcessOne />
-
-      {/* MarqueeOne */}
-      <MarqueeOne />
-
-      {/* TeamOne */}
-      <TeamOne />
 
       {/* FaqOne */}
       <div className="faq-area-1 bg-smoke space">
